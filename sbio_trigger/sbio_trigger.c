d36c64efac7ef14776a586725f689fdcfe3b2528
stelhs <stelhs@ya.ru>
2018-01-02 04:29:41 +0000

    code cleanup

--- sbio_trigger/sbio_trigger.c
+++ sbio_trigger/sbio_trigger.c
@@ -10,152 +10,137 @@
 #include <errno.h>
 #include "conf.h"
 
-#define CONFIG_FILE_NAME "/etc/sr90_sbio/triggers_input_ports.conf"
-
 struct gpio {
-    int port;
-    int fd;
-    int id;
+	int port;
+	int fd;
+	int id;
 };
 
 // list GPIO ports in ascending order
-static struct gpio inputs_list[30];
+static struct gpio inputs_list[16];
 
 void input_request(int port)
 {
-    char buf[128];
-    int fd;
+	char buf[128];
+	int fd;
 
-    snprintf(buf, sizeof buf, "%d", port);
-    fd = open("/sys/class/gpio/export", O_WRONLY);
-    write(fd, buf, strlen(buf));
-    close(fd);
+	snprintf(buf, sizeof buf, "%d", port);
+	fd = open("/sys/class/gpio/export", O_WRONLY);
+	write(fd, buf, strlen(buf));
+	close(fd);
 
-    snprintf(buf, sizeof buf, "/sys/class/gpio/gpio%d/direction", port);
-    fd = open(buf, O_WRONLY);
-    write(fd, "in", 3);
-    close(fd);
+	snprintf(buf, sizeof buf, "/sys/class/gpio/gpio%d/direction", port);
+	fd = open(buf, O_WRONLY);
+	write(fd, "in", 3);
+	close(fd);
 
-    snprintf(buf, sizeof buf, "/sys/class/gpio/gpio%d/edge", port);
-    fd = open(buf, O_WRONLY);
-    write(fd, "both", 5);
-    close(fd);
+	snprintf(buf, sizeof buf, "/sys/class/gpio/gpio%d/edge", port);
+	fd = open(buf, O_WRONLY);
+	write(fd, "both", 5);
+	close(fd);
 }
-
 
 void input_free(int port)
 {
-    char buf[128];
-    int fd;
+	char buf[128];
+	int fd;
 
-    snprintf(buf, sizeof buf, "%d", port);
-    fd = open("/sys/class/gpio/unexport", O_WRONLY);
-    write(fd, buf, strlen(buf));
-    close(fd);
+	snprintf(buf, sizeof buf, "%d", port);
+	fd = open("/sys/class/gpio/unexport", O_WRONLY);
+	write(fd, buf, strlen(buf));
+	close(fd);
 }
-
 
 void sig_handler(int signo)
 {
-    struct gpio *gp;
+	struct gpio *gp;
 
-    if (signo != SIGINT)
-        return;
+	if (signo != SIGINT)
+		return;
 
-    printf("closing all gpio files\n");
-    for (gp = inputs_list; gp->port > 0; gp++) {
-        if (!gp->fd)
-           continue;
+	printf("closing all gpio files\n");
+	for (gp = inputs_list; gp->port > 0; gp++) {
+		if (!gp->fd)
+			continue;
 
-        close(gp->fd);
-        input_free(gp->port);
-    }
-    exit(0);
-}
-
-
-void conf_setter(void *priv, char *var, char *val)
-{
-    int gpio_numbers[30];
-    int rc, i;
-    if (strcmp(var, "input-gpio-list") == 0) {
-        rc = parse_int_comma_separated(val, gpio_numbers);
-        for (i = 0; i < rc; i++)
-            inputs_list[i].port = gpio_numbers[i];
-    }
+		close(gp->fd);
+		input_free(gp->port);
+	}
+	exit(0);
 }
 
 
 int main(int argc, char **argv)
 {
-    int rc;
-    int id, max_fd;
-    char buf[128];
-    struct gpio *gp;
-    fd_set rfds;
-    char *shell_script = argv[2];
-    int value;
+	int rc;
+	int max_fd;
+	char buf[128];
+	int list_gpio[16];
+	int gpio_cnt;
+	struct gpio *gp;
+	fd_set rfds;
+	char *shell_script = argv[2];
+	int value;
+	int i;
 
-    if (argc < 3) {
-        perror("first argument must be list of triggered GPIO numbers\n");
-        perror("second argument must be path to shell script\n");
-        return -1;
-    }
+	if (argc < 3) {
+		perror("first argument must be list of triggered GPIO numbers\n");
+		perror("second argument must be path to shell script\n");
+		return -1;
+	}
 
-    signal(SIGINT, sig_handler);
+	signal(SIGINT, sig_handler);
 
-    memset(inputs_list, 0, sizeof inputs_list);
-/*    rc = parse_config(CONFIG_FILE_NAME, NULL, conf_setter);
-    if (rc < 0) {
-        printf("Can't get config\n");
-        return -1;
-    }*/
+	memset(inputs_list, 0, sizeof inputs_list);
+	gpio_cnt = parse_int_comma_separated(argv[1], list_gpio);
+	if (gpio_cnt < 1) {
+		perror("incorrect GPIO list\n");
+		return -1;
+	}
 
-    if (!inputs_list[0].port) {
-        printf("Incorrect input-gpio-list parameter\n");
-        return -1;
-    }
+	for (i = 0; i < gpio_cnt; i++) {
+		gp = inputs_list + i;
+		gp->port = list_gpio[i];
+		gp->id = i + 1;
 
-    id = 1;
-    for (gp = inputs_list; gp->port > 0; gp++) {
-        gp->id = id++;
-        input_free(gp->port);
-        input_request(gp->port);
-        snprintf(buf, sizeof buf, "/sys/class/gpio/gpio%d/value", gp->port);
-        gp->fd = open(buf, O_RDONLY);
-        lseek(gp->fd, 0, SEEK_SET);
-        read(gp->fd, buf, sizeof buf);
-    }
-    
-    for (;;) {
-        FD_ZERO(&rfds);
-        max_fd = 0;
-        for (gp = inputs_list; gp->port > 0; gp++) {
-            if (gp->fd > max_fd)
-                max_fd = gp->fd;
-            FD_SET(gp->fd, &rfds);
-        }
+		input_free(gp->port);
+		input_request(gp->port);
+		snprintf(buf, sizeof buf, "/sys/class/gpio/gpio%d/value", gp->port);
+		gp->fd = open(buf, O_RDONLY);
+		lseek(gp->fd, 0, SEEK_SET);
+		read(gp->fd, buf, sizeof buf);
+	}
 
-        rc = select(max_fd + 1, NULL, NULL, &rfds, NULL);
-        if (rc == -1) {
-           fprintf(stderr, "select error %d", errno);
-           continue;
-        }
+	for (;;) {
+		FD_ZERO(&rfds);
+		max_fd = 0;
+		for (gp = inputs_list; gp->port > 0; gp++) {
+			if (gp->fd > max_fd)
+				max_fd = gp->fd;
+			FD_SET(gp->fd, &rfds);
+		}
 
-        for (gp = inputs_list; gp->port > 0; gp++) {
-            if (!FD_ISSET(gp->fd , &rfds))
-                continue;
+		rc = select(max_fd + 1, NULL, NULL, &rfds, NULL);
+		if (rc == -1) {
+			fprintf(stderr, "select error %d", errno);
+			continue;
+		}
 
-            lseek(gp->fd, 0, SEEK_SET);
-            read(gp->fd, buf, 3);
-            sscanf(buf, "%d", &value);
+		for (gp = inputs_list; gp->port > 0; gp++) {
+			if (!FD_ISSET(gp->fd, &rfds))
+				continue;
 
-            printf ("active %d = %d\n", gp->id, value);
-            snprintf(buf, sizeof buf, "%s %d %d", shell_script, gp->id, value);
-            system(buf);
-        }
-    }
+			lseek(gp->fd, 0, SEEK_SET);
+			read(gp->fd, buf, 3);
+			sscanf(buf, "%d", &value);
 
-    return 0;
+			printf("active %d = %d\n", gp->id, value);
+			snprintf(buf, sizeof buf, "%s %d %d", shell_script,
+					gp->id, value);
+			system(buf);
+		}
+	}
+
+	return 0;
 }
 
